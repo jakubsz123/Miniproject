23060655e8e879c10bf40a3cee63749106e5113a
martinkarlik <mkarli18@student.aau.dk>
2019-11-24 13:21:35 +0100

    Added server dependencies.

--- CSS_Server/main.cpp
+++ CSS_Server/main.cpp
@@ -1,3 +1,22 @@
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <thread>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+#define BACKLOG 10
+
+
 int main() {
 
 };
