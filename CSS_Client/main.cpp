69c16cda598689fd8366876be9b3e6c03b060e71
jakubsz123 <48472503+jakubsz123@users.noreply.github.com>
2019-11-24 13:39:15 +0100

    incorporating the game

--- CSS_Client/main.cpp
+++ CSS_Client/main.cpp
@@ -15,6 +15,43 @@
 
 #define DEFAULT_ADDRESS "localhost"
 
+//function that chooses the winner based on the server's and client's choice; 1: win, 0: draw, -1: lose
+int decideWinner(std::string me, const char* opponent) {
+	if (me == "rock") {
+		if (opponent[0] == 'r') {
+			return 0;
+		}
+		else if (opponent[0] == 'p') {
+			return -1;
+		}
+		else {
+			return 1;
+		}
+	}
+	else if (me == "paper") {
+		if (opponent[0] == 'p') {
+			return 0;
+		}
+		else if (opponent[0] == 's') {
+			return -1;
+		}
+		else {
+			return 1;
+		}
+	}
+	else {
+		if (opponent[0] == 's') {
+			return 0;
+		}
+		else if (opponent[0] == 'r') {
+			return -1;
+		}
+		else {
+			return 1;
+		}
+	}
+}
+
 int main(int argc, char* argv[]) // makes it able to input command lines
 {
 	// start up the winsocket
@@ -39,16 +76,27 @@ int main(int argc, char* argv[]) // makes it able to input command lines
 	//string s(argv[1]);
 	char buf[DEFAULT_BUFLEN];
 
-	bool running = true;
-	while (running == true) {
+	bool gameOver = false;
+
+	while (!gameOver) {
 		// Write out that socket
-		std::string s = "";
-		std::cin.get >> s;
+
+		std::string selection = "";
+
+		while (selection != "paper" && selection != "rock" && selection != "scissors" && selection != "quit") {
+			std::cout << "Choose: 'rock' / 'paper' / 'scissors' (or 'quit' to quit)." << std::endl;
+			std::cin >> selection;
+		}
+
+		std::string s(selection);
+
 		int sendOK = sendto(out, s.c_str(), s.size, 0, (sockaddr*)&server, serverLength);
 
 		if (sendOK == SOCKET_ERROR) {
 			std::cout << "That didn't work" << WSAGetLastError();
 		}
+
+		std::cout << "It's opponent's turn, please wait..." << std::endl;
 
 		ZeroMemory(buf, DEFAULT_BUFLEN);
 		int bytesIn = recvfrom(out, buf, DEFAULT_BUFLEN, 0, (sockaddr*)&server, &serverLength);
@@ -58,8 +106,20 @@ int main(int argc, char* argv[]) // makes it able to input command lines
 			continue;
 		}
 
-		if (s == "quit") {
-			running = false;
+		if (buf[0] == 'q' || selection == "quit") {
+			gameOver = true;
+		}
+		else {
+			int result = decideWinner(selection, buf);
+			if (result == -1) {
+				std::cout << "The opponent chose " << buf << ". That beats " << selection << ", so you lose." << std::endl;
+			}
+			else if (result == 1) {
+				std::cout << "The opponent chose " << buf << ", but that was wrong, you win. Well done. Good job. You're amazing." << std::endl;
+			}
+			else {
+				std::cout << "It's a draw, because you chose the same thing as the opponent. You draw." << std::endl;
+			}
 		}
 	}
 
